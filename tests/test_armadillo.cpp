3dabeae8ed312bcde90266f0c4a911f58321bc56
Matteo Limberto <ml@bendingspoons.com>
2014-12-17 08:52:59 +0100

    Update test_armadillo.cpp

--- tests/test_armadillo.cpp
+++ tests/test_armadillo.cpp
@@ -10,7 +10,16 @@ int main(int argc,char **argv)
 	mat A = randu<mat>(4,5);
 	mat B = randu<mat>(4,5);
 
-	cout << A*B.t() << endl;
+	// cout << A*B.t() << endl;
+	cout << A << endl;
+
+	B = cumsum(A);
+
+	cout << B << endl;
+
+	SpMat<double> V(5,5);
+
+	cout << V << endl; 
 
 	return 0;
 }
