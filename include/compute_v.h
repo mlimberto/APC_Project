ffd564f856ff292cf8a9187464f6372c027aee71
Matteo Limberto <ml@bendingspoons.com>
2014-12-18 18:32:15 +0100

    Added compute_v class definition

---
+++ include/compute_v.h
@@ -0,0 +1,39 @@
+//
+//  compute_v.h
+//  
+//  Class for the simil-projected gradient solver
+//  for the matrix V
+
+#ifndef COMPUTE_V_H
+#define COMPUTE_V_H
+
+#include <iostream>
+#include <armadillo>
+
+using namespace arma;
+
+
+
+class Compute_V {
+
+
+
+
+public :
+
+	Compute_V(); // Constructor
+
+	void solve();
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif /* defined(COMPUTE_V_H) */
